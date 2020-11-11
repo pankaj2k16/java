a9aed93f9c18e99464a50a26641752a17ace12c2
Pankaj Kumar <forpankaj.prasar@gmail.com>
2020-11-11 07:49:34 +0530

    Hello first Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+       Person person = new Person("Pankaj");
+        System.out.println(person.getName());
+    }
+}
